e1de5d66aed3ffb4a1ac5e470b38a921df3a2fcd
Wilker Lucio <wilkerlucio@gmail.com>
2024-08-25 12:59:02 -0300

    Logs and keep storage directory

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import {WebSocketServer} from 'ws';
+import {createFilePersister} from 'tinybase/persisters/persister-file';
+import {createMergeableStore} from 'tinybase';
+import {createWsServer} from 'tinybase/synchronizers/synchronizer-ws-server';
+
+const PORT = 8047
+
+const server = createWsServer(
+  new WebSocketServer({port: PORT}),
+  (pathId) => {
+    console.log(`Connecting on persister for path ${pathId}`);
+
+    return [
+      createFilePersister(
+        createMergeableStore(),
+        'server-storage/' + pathId.replace(/[^a-zA-Z0-9]/g, '-') + '.json',
+      ),
+      (store) => store.setValue('pathId', pathId),
+    ]
+  },
+);
+
+console.log(`Server started on port ${PORT}`);
